88a15509117a00fbf793caa290ba0b0ef5b682b0
Nohaelsayedd <nohasayed1311@gmail.com>
2025-09-30 08:19:49 +0300

    Working Launch file

--- launch/shapes.launch.py
+++ launch/shapes.launch.py
@@ -4,9 +4,9 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='my_turtle_shapes',
-            executable='shape_node',
-            name='shape_node',
+            package='turtlesim',
+            executable='turtlesim_node',
+            name='turtlesim',
             output='screen'
         ),
         Node(
@@ -15,11 +15,4 @@ def generate_launch_description():
             name='turtle_commander',
             output='screen'
         ),
-        # Optionally add turtlesim (uncomment if you want a full launch)
-        # Node(
-        #     package='turtlesim',
-        #     executable='turtlesim_node',
-        #     name='turtlesim',
-        #     output='screen'
-        # ),
     ])
